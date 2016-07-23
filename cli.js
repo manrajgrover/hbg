94d0f8fe184381820b8593829bb61b67da6eb92c
Manraj Singh <manrajsinghgrover@gmail.com>
2016-07-23 19:06:35 +0530

    Code refactored

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@
 * @Author: Manraj Singh
 * @Date:   2016-07-10 20:00:15
 * @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-07-23 11:48:22
+* @Last Modified time: 2016-07-23 19:04:12
 */
 
 'use strict';
@@ -15,6 +15,22 @@ const ora = require('ora');
 const chalk = require('chalk');
 const config = require('./config');
 const path = require('path');
+
+const getPath = (folderPath, name) => {
+  return path.resolve(folderPath, name);
+}
+
+const getExtension = (lang) => {
+
+}
+
+const generate = (folderPath, question) => {
+  let filePath = getPath(folderPath, question.toString());
+  let inputPath = getPath(folderPath, 'input.txt');
+  let outputPath = getPath(folderPath, 'output.txt');
+  fs.mkdirSync(folderPath);
+  fs.writeFileSync(i.toString());
+}
 
 const argv = yargs
   .usage('sudo $0 <command>')
@@ -27,9 +43,10 @@ const argv = yargs
       .example('sudo $0 gen') // To-do
       .argv;
     let { l, q } = argv;
+    let ext = getExtension(l);
     for(let i = 1;i <= q; i++) {
       let folderPath = path.resolve(__dirname, i.toString());
-      fs.mkdirSync(folderPath);
+
     }
   })
   .command('config', 'Change config file', (yargs) => {
