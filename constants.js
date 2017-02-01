b7e92d05881b390d34f705d7e34bc2b01a8bc99e
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-02-01 22:39:21 +0530

    Fix: Constants moved to separate file

---
+++ constants.js
@@ -0,0 +1,9 @@
+const LANG_ERR = 'Not a valid language. Please run `hbg config -l` to get the list of languages.';
+const QUES_ERR = 'Not a valid question number. Please enter a number.';
+const TASK_COMPL = 'Task completed. Support project at https://github.com/ManrajGrover/hbg.';
+
+module.exports = {
+  LANG_ERR,
+  QUES_ERR,
+  TASK_COMPL,
+};
